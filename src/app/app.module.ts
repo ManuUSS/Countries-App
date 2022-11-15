5ac5c774e4520a668bc7cc85be193a5f4f667812
Manuel Ulate <fr3zs09@gmail.com>
2022-11-15 09:16:43 -0600

    modules and components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CountryModule } from './modules/country/country.module';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CountryModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
