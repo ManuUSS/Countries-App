0bea681e644bc19d0ae6e9d89f229f1b4a5e6369
Manuel Ulate <fr3zs09@gmail.com>
2022-11-18 09:54:12 -0600

    httpRequest optimizated

--- src/app/modules/country/services/country.service.ts
+++ src/app/modules/country/services/country.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CountryResponse } from '../interfaces/country.interface';
+import { tap } from 'rxjs/operators';
 
 @Injectable({
     providedIn: 'root'
@@ -9,27 +10,29 @@ import { CountryResponse } from '../interfaces/country.interface';
 export class CountryService {
 
     private apiUrl: string = 'https://restcountries.com/v3.1';
+    private usedFields = 'name,capital,flags,population,cca2';
+    private params = new HttpParams().set( 'fields', this.usedFields );
 
     constructor( private http: HttpClient ) { }
 
     public searchCountry( term: string ):Observable<CountryResponse[]> {
 
         const url = `${ this.apiUrl }/name/${ term }`;
-        return this.http.get<CountryResponse[]>( url );
+        return this.http.get<CountryResponse[]>( url, { params:this.params } );
 
     }
 
     public searchCapital( term: string ):Observable<CountryResponse[]> {
             
         const url = `${ this.apiUrl }/capital/${ term }`;
-        return this.http.get<CountryResponse[]>( url );
+        return this.http.get<CountryResponse[]>( url, { params:this.params } );
     
     }
 
     public searchRegion( term: string ):Observable<CountryResponse[]> {
             
         const url = `${ this.apiUrl }/region/${ term }`;
-        return this.http.get<CountryResponse[]>( url );
+        return this.http.get<CountryResponse[]>( url, { params:this.params } );
     
     }
 
